4f6a5e5e700be0023eeb9ca26bbea8dcb29fa8f2
patrickgrey <websemantic@yahoo.co.uk>
2015-10-13 16:40:05 +0100

    0.0.1 Project reset. Basic HTML and simple styles

--- app/scripts/modules/app.js
+++ app/scripts/modules/app.js
@@ -1,26 +1,4 @@
 var infoButton = require('./infoButton.js'),
     _ = require('underscore');
 
-var containerDiv = document.getElementById('ft-container');
-var contentDiv = document.getElementById('ft-content-container');
-var infoDiv = document.getElementById('ft-information-container');
-var infoButtonElement = infoButton();
-var toggleInfo = function (event) {
-    infoDiv.className.indexOf('ft-screen-reader-text') === -1 ? hideInfo() : showInfo();
-
-};
-var showInfo = function (event) {
-    infoDiv.className = 'ft-information';
-    infoButtonElement.innerHTML = "x";
-    infoButtonElement.className = "ft-button-information ft-button-information-close";
-    // Change text to cross and colour to red.
-};
-var hideInfo = function (event) {
-    infoDiv.className = 'ft-information ft-screen-reader-text';
-    infoButtonElement.innerHTML = "?";
-    infoButtonElement.className = "ft-button-information";
-};
-infoButtonElement.onclick = toggleInfo;
-// infoButtonElement.onfocus = showInfo;
-// infoButtonElement.onblur = hideInfo;
-containerDiv.insertBefore(infoButtonElement, contentDiv);
+// var infoButtonElement = infoButton('ft-container', 'ft-content-container', 'ft-information-container');
--- app/scripts/modules/infoButton.js
+++ app/scripts/modules/infoButton.js
@@ -1,6 +1,33 @@
-module.exports = function () {
-    var button = document.createElement('button');
+module.exports = function (infoContainer, infoParent, infoPanel) {
+
+    var containerDiv = document.getElementById(infoContainer),
+    contentDiv = document.getElementById(infoParent),
+    infoDiv = document.getElementById(infoPanel),
+    button = document.createElement('button');
+
     button.innerHTML = '?';
     button.className = 'ft-button-information';
+
+    var toggleInfo = function (event) {
+        infoDiv.className.indexOf('ft-screen-reader-text') === -1 ? hideInfo() : showInfo();
+    };
+
+    var showInfo = function (event) {
+        infoDiv.className = 'ft-information';
+        button.innerHTML = "x";
+        button.className = "ft-button-information ft-button-information-close";
+        // Change text to cross and colour to red.
+    };
+
+    var hideInfo = function (event) {
+        infoDiv.className = 'ft-information ft-screen-reader-text';
+        button.innerHTML = "?";
+        button.className = "ft-button-information";
+    };
+
+    button.onclick = toggleInfo;
+
+    containerDiv.insertBefore(button, contentDiv);
+
     return button;
 }

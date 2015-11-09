da009b0f0781cdf80dc45047c9009ac6ed3bd661
patrickgrey <websemantic@yahoo.co.uk>
2015-11-09 15:56:28 +0000

    Wasted a day trying to get GSAP to work with browserify. Gave up and loaded in page using Bower

--- app/scripts/modules/app.js
+++ app/scripts/modules/app.js
@@ -1,6 +1,16 @@
 var infoButton = require('./infoButton.js'),
     activateAccessibleControls = require('./activateAccessibleControls.js'),
+    // gsap = require('gsap'),
+    // TweenLite = require('https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TimelineLite.min.js'),
+    // Draggable = require('../../../node_modules/gsap/src/uncompressed/utils/Draggable.js'),
+    // CSSPlugin = require('../../../node_modules/gsap/src/uncompressed/plugins/CSSPlugin.js'),
+    // CSSPlugin = require('./../bower_components/gsap/src/uncompressed/plugins/CSSPlugin.js'),
+    // Draggable = require('./../bower_components/gsap/src/uncompressed/utils/Draggable.js'),
     _ = require('underscore');
+    
+    
+    // TweenLite.to("#ft-audio-player-custom-btn-time-id", 1, {x:100, y:100, scale:0.5, rotation:180, skewX:45});
+    Draggable.create("#ft-audio-player-custom-btn-time-id", {type: "rotation", throwProps: true});
 
 
 var infoButtonElement = infoButton('ft-wrapper-id', 'ft-content-id', 'ft-information-id');
